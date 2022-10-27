ee5642c1f91677c6227bd9a957b37d6f60d9111e
NathaelB <kirua48000@gmail.com>
2022-10-27 21:31:30 +0200

    :construction: add function general and include syntaxe

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,23 @@
 #use "types.ml";;
 
-let f = Equ(And(Symb "a", Symb "c"),Or(Not(Symb "b"),Imp(Symb "c",And(Bot,Top))));;
-let f1 = Equ(And(Symb "a", Symb "b"), Or(Not(Symb "a"), Symb "b"));;
+let priority: prop -> int = function
+  | Equ(g,d) -> 0
+  | Imp(g,d) -> 1
+  | Or (g,d) -> 2
+  | And(g,d) -> 3
+  | Not(a) -> 4
+  | _ -> 5
+;;
+
+let affiche_symb: prop -> string = function
+  | Symb a ->  a
+  | Top ->  "⊤"
+  | Bot ->  "⊥"
+  | Not _ ->  "¬"
+  | And (_,_) ->"∧"
+  | Or (_,_) -> "∨"
+  | Imp (_,_) ->"⇒"
+  | Equ (_,_) -> "⇔"
+;;
+
+#use "syntaxe.ml";;
